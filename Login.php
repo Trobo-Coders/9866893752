e50a615eb4a6f02f48958ae40187a4b70e07cf3c
Argent-AR <80090163+Argent-AR@users.noreply.github.com>
2021-09-19 18:27:35 +0530

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,9 @@
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $uname = test_input($_POST["uname"]);
+  $psw = test_input($_POST["psw"]);
+}
+
+if ($uname = "Ruthvik Anne" && $psw = "Ar")
+{
+    window.location.replace("https://trobo-coders.github.io/9866893752/Home.html");
+}
