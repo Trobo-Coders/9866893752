796b28279e5972d66ea21a74a5505e83a87b1529
Argent-AR <80090163+Argent-AR@users.noreply.github.com>
2021-10-06 12:33:56 +0530

    Modification

--- Login.php
+++
@@ -1,9 +0,0 @@
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $uname = test_input($_POST["uname"]);
-  $psw = test_input($_POST["psw"]);
-}
-
-if ($uname = "Ruthvik Anne" && $psw = "Ar")
-{
-    window.location.replace("https://trobo-coders.github.io/9866893752/Home.html");
-}
